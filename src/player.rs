98f63dd9b2ca239365d4d59475a5b04ca8f5d183
Chris Pinkney <hey@chrispinkney.com>
2021-11-16 21:45:57 -0500

    added wsad controls

--- src/player.rs
+++ src/player.rs
@@ -26,10 +26,14 @@ impl Player {
     pub fn update(&mut self, ctx: &mut BTerm, map: &Map) {
         if let Some(key) = ctx.key {
             let delta = match key {
-                VirtualKeyCode::Left => Point::new(-1, 0),
-                VirtualKeyCode::Right => Point::new(1, 0),
                 VirtualKeyCode::Up => Point::new(0, -1),
                 VirtualKeyCode::Down => Point::new(0, 1),
+                VirtualKeyCode::Left => Point::new(-1, 0),
+                VirtualKeyCode::Right => Point::new(1, 0),
+                VirtualKeyCode::W => Point::new(0, -1),
+                VirtualKeyCode::S => Point::new(0, 1),
+                VirtualKeyCode::A => Point::new(-1, 0),
+                VirtualKeyCode::D => Point::new(1, 0),
                 _ => Point::zero()
             };
 
