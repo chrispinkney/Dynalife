f4b7a0d3405e0f489d3bdf9e628e7b7f428a6b3d
Chris Pinkney <hey@chrispinkney.com>
2021-11-17 15:36:24 -0500

    fully implemented walls and corridors

--- src/map_builder.rs
+++ src/map_builder.rs
@@ -9,6 +9,21 @@ pub struct MapBuilder {
 }
 
 impl MapBuilder {
+    pub fn new(rng: &mut RandomNumberGenerator) -> Self {
+        let mut mb = MapBuilder {
+            map: Map::new(),
+            rooms: Vec::new(),
+            player_start: Point::zero()
+        };
+
+        mb.fill(TileType::Wall);
+        mb.build_random_rooms(rng);
+        mb.build_corridors(rng);
+        mb.player_start = mb.rooms[0].center();
+        mb
+    }
+
+
     /// Iterate over all tiles in the tiles vector, changing all tiles into a wall.
     fn fill(&mut self, tile: TileType) {
         self.map.tiles.iter_mut().for_each(|t| *t = tile);
